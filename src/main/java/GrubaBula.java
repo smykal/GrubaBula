b33dde558e94a1ccf39c8b57d093b043a6e17483
mateusz <smykal@engineer.com>
2021-02-27 14:57:52 +0100

    add builder for Pizza

--- src/main/java/GrubaBula.java
+++ src/main/java/GrubaBula.java
@@ -2,7 +2,14 @@ import java.util.ArrayList;
 
 public class GrubaBula {
     public static void main(String[] args) {
-        Pizza pizza = new Pizza("Pizza", Dough.MEDIUM, Sauce.GARLIC, Size.MEDIUM, new ArrayList<>());
+        Pizza pizza = new Pizza.PizzaBuilder()
+                .withName("name for pizza like in Sturbacks")
+                .withDough(Dough.MEDIUM)
+                .withSauce(Sauce.CREAM)
+                .withTopping(new ArrayList<>())
+                .withSize(Size.LARGE)
+                .build();
+
         System.out.println(pizza.toString());
     }
 }
--- src/main/java/Pizza.java
+++ src/main/java/Pizza.java
@@ -7,12 +7,50 @@ public class Pizza {
     private Size size;
     private List<Topping> topping;
 
-    public Pizza(String name, Dough dough, Sauce sauce, Size size, List<Topping> topping) {
-        this.name = name;
-        this.dough = dough;
-        this.sauce = sauce;
-        this.size = size;
-        this.topping = topping;
+    public static final class PizzaBuilder {
+        private String name;
+        private Dough dough;
+        private Sauce sauce;
+        private Size size;
+        private List<Topping> topping;
+
+        PizzaBuilder() {
+        }
+
+        public PizzaBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PizzaBuilder withDough(Dough dough) {
+            this.dough = dough;
+            return this;
+        }
+
+        public PizzaBuilder withSauce(Sauce sauce) {
+            this.sauce = sauce;
+            return this;
+        }
+
+        public PizzaBuilder withSize(Size size) {
+            this.size = size;
+            return this;
+        }
+
+        public PizzaBuilder withTopping(List<Topping> topping) {
+            this.topping = topping;
+            return this;
+        }
+
+        public Pizza build() {
+            Pizza pizza = new Pizza();
+            pizza.topping = this.topping;
+            pizza.sauce = this.sauce;
+            pizza.dough = this.dough;
+            pizza.size = this.size;
+            pizza.name = this.name;
+            return pizza;
+        }
     }
 
     @Override
